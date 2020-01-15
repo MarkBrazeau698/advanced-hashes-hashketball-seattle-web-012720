91e10c8031993bf161556f89cd065a2d42e33998
Mark Brazeau <kitsapbackflow@gmail.com>
2020-01-15 22:54:42 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -275,7 +275,7 @@ def player_with_longest_name
   name
 end
 
-def long_name_steals_a_ton(player_with_longest_name, name)
+def long_name_steals_a_ton?
   most_steals = 0
   name = " "
   game_hash.each do |place, team|
